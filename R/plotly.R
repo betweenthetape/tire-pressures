0e8a55f230e688d9a4493c0ed853e2ccb15c62ae
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2024-11-13 15:38:43 +0100

    Add fun

---
+++ R/plotly.R
@@ -0,0 +1,27 @@
+#' @noRd
+#' @keywords internal
+plotly_low_config <- function(plot) {
+  ggplotly(plot) |>
+    config(
+      displayModeBar = TRUE,
+      displaylogo = FALSE,
+      modeBarButtonsToRemove = list(
+        "zoom",
+        # "pan",
+        "select",
+        # "zoomIn",
+        # "zoomOut",
+        "autoScale",
+        # "resetScale",
+        "lasso2d",
+        "hoverClosestCartesian",
+        "hoverCompareCartesian"
+      ),
+      # Download button
+      toImageButtonOptions = list(
+        height = NULL,
+        width = NULL,
+        scale = 6
+      )
+    )
+}
